e2b9d513e2c043c12b4780bc6cf1d8838821cb86
Yuuki <82380343+xcscxr@users.noreply.github.com>
2022-02-11 16:16:23 +0530

    Create droplink_bypass.py

---
+++ droplink_bypass.py
@@ -0,0 +1,41 @@
+
+import re
+import time
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+
+
+# droplink url
+url = ""
+
+# ==============================================
+    
+def droplink_bypass(url):
+    client = requests.Session()
+    res = client.get(url)
+
+    ref = re.findall("action[ ]{0,}=[ ]{0,}['|\"](.*?)['|\"]", res.text)[0]
+
+    h = {'referer': ref}
+    res = client.get(url, headers=h)
+
+    bs4 = BeautifulSoup(res.content, 'lxml')
+    inputs = bs4.find_all('input')
+    data = { input.get('name'): input.get('value') for input in inputs }
+
+    h = {
+        'content-type': 'application/x-www-form-urlencoded',
+        'x-requested-with': 'XMLHttpRequest'
+    }
+    p = urlparse(url)
+    final_url = f'{p.scheme}://{p.netloc}/links/go'
+
+    time.sleep(3.1)
+    res = client.post(final_url, data=data, headers=h).json()
+
+    return res
+
+# ==============================================
+
+print(droplink_bypass(url))
